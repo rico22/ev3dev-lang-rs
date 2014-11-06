ee3b36e5757da2cbd2ca89afa88ff41921d63ea8
Rick Poyner <rico@jaybridge.com>
2014-11-06 10:32:56 -0500

    WIP on device::connect() and unit test. doesn't work yet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-#[cfg(test)]
-extern crate hamcrest;
+use std::collections::HashSet;
+use std::collections::HashMap;
+use std::io::fs;
+use std::io::fs::PathExtensions;
+use std::io::{File, IoResult, USER_RWX};
+
+pub type Matches = HashSet<String>;
+pub type AttributeMatches = HashMap<String, Matches>;
 
 #[allow(dead_code)]
 pub struct InputPort(&'static str);
@@ -22,6 +28,27 @@ struct Device {
 }
 
 impl Device {
+    fn new() -> Device {
+        Device { path: String::new(), device_index: -1 }
+    }
+
+    fn get_attr_string(&self, name: &str) -> String {
+        let mut f = File::open(&Path::new(self.path + name));
+        f.read_to_string().ok().expect("no device connected")
+    }
+
+    fn connect(&mut self, dir: &str, pattern: &str,
+               match_spec: AttributeMatches) -> bool {
+        let mut paths = match fs::walk_dir(&Path::new(dir)) {
+            Err(_) => { return false; }
+            Ok(paths) => paths,
+        };
+        for path in paths.filter(|e| { true }) {
+            println!("{}", path.display());
+        }
+        // stub
+        false
+    }
 }
 
 pub struct Sensor {
@@ -31,12 +58,31 @@ pub struct Sensor {
 impl Sensor {
     pub fn new(port: InputPort) -> Sensor {
         // stub.
-        Sensor { dev: Device {path:"".to_string(), device_index:-1} }
+        Sensor { dev: Device::new() }
     }
 }
 
-#[test]
+#[cfg(test)]
+mod test {
+    extern crate hamcrest;
+    use super::Device;
+    use std::collections::{HashSet, HashMap};
+    
+    #[test]
+    fn try_types() {
+        let mut matches = HashSet::new();
+        matches.insert("Linux");
+    }
 
-fn it_works() {
-
+    #[test]
+    fn device_connect() {
+        let mut dut = Device::new();
+        let mut matchy = HashMap::new();
+        let mut matches = HashSet::new();
+        matches.insert("in1".to_string());
+        matchy.insert("port_name".to_string(), matches);
+        let data_dir = Path::new(file!()).dir_path().dir_path().join("data");
+        let sensor_dir = data_dir.join_many(&["sys", "class", "msensor"]);
+        assert!(dut.connect(sensor_dir.as_str().expect(""), "sensor", matchy));
+    }
 }
