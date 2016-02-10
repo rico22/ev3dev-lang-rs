7fefcba088cb647d7b961e84c376f3f3d2f17c7b
Rick Poyner <rico@alum.mit.edu>
2016-02-10 16:20:12 -0500

    autogen: add sensor class description.

--- src/sensor.rs
+++ src/sensor.rs
@@ -6,6 +6,24 @@ use std::collections::HashSet;
 static SENSOR_CLASS_DIR: &'static str = "sys/class/msensor";
 static SENSOR_PATTERN: &'static str = "sensor";
 
+//~autogen generic-class-description classes.sensor>currentClass
+
+/// The sensor class provides a uniform interface for using most of the
+/// sensors available for the EV3. The various underlying device drivers will
+/// create a `lego-sensor` device for interacting with the sensors.
+/// 
+/// Sensors are primarily controlled by setting the `mode` and monitored by
+/// reading the `value<N>` attributes. Values can be converted to floating point
+/// if needed by `value<N>` / 10.0 ^ `decimals`.
+/// 
+/// Since the name of the `sensor<N>` device node does not correspond to the port
+/// that a sensor is plugged in to, you must look at the `address` attribute if
+/// you need to know which port a sensor is plugged in to. However, if you don't
+/// have more than one sensor of each type, you can just look for a matching
+/// `driver_name`. Then it will not matter which port a sensor is plugged in to - your
+/// program will still work.
+
+//~autogen
 pub struct Sensor {
     dev: Device,
     port_name: String,
