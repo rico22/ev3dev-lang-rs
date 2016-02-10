3406975f3c19a4076f7d852a40b83298699fd582
Rick Poyner <rico@jaybridge.com>
2016-02-10 14:49:00 -0500

    fix broken crate name, and begin to document.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,24 @@
+//! ev3dev_lang_rs -- Rust language bindings for ev3dev.org systems
+//!
+//! The ev3dev_lang_rs crate provides an ev3dev-lang standards-conforming API
+//! for access to sensors and actuators of EV3 and similar robot systems.
+
 // #![feature(plugin)]
 
 // #![plugin(clippy)]
 
+/// The system module provides a basis for specializing the file system for
+/// testing.
 pub mod system;
 
+/// The testbase module provides helpers for unit testing.
 #[macro_use]
 pub mod testbase;
+
+/// The device module provides a standards-conforming Device struct, with
+/// associated types and traits.
 pub mod device;
+
+/// The sensor modules provides standards-conforming Sensor APIs.
 pub mod sensor;
 
